72e395cc4cde5cea25de26aa08edd264745e64c2
rodrigoDevyenn <dev.rodrigosleite@gmail.com>
2025-10-30 15:21:43 -0300

    Codigos dos exercicios 1 e 2 da atividade 1 da semana 10

---
+++ Sem10-T1/sem10-t1-ex01.py
@@ -0,0 +1,10 @@
+
+def loop():
+    for i in range(50):
+        print(i + 1)
+
+def main():
+    loop()
+
+if __name__ == '__main__':
+    main()
---
+++ Sem10-T1/sem10-t1-ex02.py
@@ -0,0 +1,29 @@
+
+def ehPar(n):
+    return n % 2 == 0
+
+def contadorPar(x):
+    if x == True:
+        return 1
+    else:
+        return 0
+
+def contadorImpar(x):
+    if x == True:
+        return 0
+    else:
+        return 1
+
+def main():
+    par = 0
+    impar = 0
+    for i in range(100):
+        num = int(input(f'Insira o número {i + 1}º: '))
+        par = par + contadorPar(ehPar(num))
+        impar = impar + contadorImpar(ehPar(num))
+
+    print(par)
+    print(impar)
+
+if __name__ == '__main__':
+    main()
